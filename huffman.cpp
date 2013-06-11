4d2cbfa4325c1047a23ee2c54fbff5606b1993bd
Ning Ke <ningske@gmail.com>
2013-06-11 06:22:23 -0400

    Add some comments.

--- huffman.cpp
+++ huffman.cpp
@@ -1,3 +1,17 @@
+/**
+ * This is an implementation of Huffman coding.
+ *
+ * The core algorithm is taken from the CLR book (Introduction of Algorithms),
+ * Chapter 16.3, and directly used to implement the 'build_tree()' routine.
+ *
+ * After the tree is built, a code table that maps a character to a binary
+ * code is built from the tree, and used for encoding text. Decoding is done
+ * by traversing the Huffman tree, as prescribed by the algorithm.
+ *
+ * Binary codes are represented by std::vector<bool>, which is a specialized
+ * vector that optimizes space.
+ */
+
 #include <vector>
 #include <queue>
 #include <map>
@@ -61,6 +75,9 @@ HuffmanTree *build_tree(vector< pair<char, unsigned> > &alph) {
     return root;
 }
 
+/**
+ * Prints the tree nodes in breadth-first order
+ */
 void print_tree(HuffmanTree *t) {
     deque< pair<HuffmanTree *, int> > q;
 
